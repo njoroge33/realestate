ecf5cdc13711e85a1bf13af70a8b0470cc61b9ce
njoroge33 <johngichuhi769@gmail.com>
2020-01-21 16:38:21 +0300

    create a users api

--- estate/views.py
+++ estate/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, status
+from .serializers import UserSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
