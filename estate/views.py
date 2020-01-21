aaee9c13817ae8aab677c1c11fb748004862dfa7
njoroge33 <johngichuhi769@gmail.com>
2020-01-21 16:52:34 +0300

    create a post request for users api

--- estate/views.py
+++ estate/views.py
@@ -7,3 +7,10 @@ from rest_framework.response import Response
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
